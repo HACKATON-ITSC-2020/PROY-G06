167177551616d2e8de889ae4a5757aad87741ee6
ferchusmh <66027731+ferchusmh@users.noreply.github.com>
2020-10-31 11:24:02 -0300

    Formulario principal

--- Vitas-Stock/Form1.Designer.cs
+++ Vitas-Stock/Form1.Designer.cs
@@ -60,6 +60,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(137, 20);
             this.textBox1.TabIndex = 1;
+            
             // 
             // textBox2
             // 
